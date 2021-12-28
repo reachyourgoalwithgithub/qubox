c43124e654d0f7fd84977fe5fe48243b2068f5d7
reachyourgoalwithgithub <96760319+reachyourgoalwithgithub@users.noreply.github.com>
2021-12-28 10:09:15 +0530

    Add files via upload

---
+++ Chat - Qubox/README.md
@@ -0,0 +1,21 @@
+Project Details: 
+
+    [+] Create NPM project - npm init 
+
+    [+] Create index.html
+    
+    [+] Create public folder add style.css and client.js files
+
+    [+] Install dependancies. express and nodemon
+
+    [+] Create a express server (server.js)
+
+    [+] do frontend part
+
+    [+] Install socket.io, Setup socket.io in server.js and client client.js
+
+    [+] Client send message logic in client.js
+
+    [+] Recieve message on server and broadcast to all clients
+
+    [+] Recieve message on client and display it. scrollToBottom etc...
---
+++ Chat - Qubox/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+    socket.on('message', (msg) => {
+        //Broadcasting means sending a message to everyone
+        socket.broadcast.emit('message', msg)
+    })
+
+})
